27027679dde2f90ebb853ab6e4fd48ec21ac4d78
landing-russia <landing-russia@yandex.ru>
2020-12-26 00:11:02 +0300

    design nav v1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="mx-auto my-12 space-x-5 flex flex-wrap items-center justify-center">
-    <a class="px-5 py-2 text-lg bg-amber-400 dark:bg-amber-600 text-gray-900 dark:text-white rounded-full font-semibold transition duration-150 ease-in-out"
+<div class="mx-auto my-10 space-x-7 flex flex-wrap items-center justify-center">
+    <a class="text-xl leading-tight border-b-4 border-amber-400 dark:border-amber-600 text-gray-900 dark:text-white font-semibold transition duration-150 ease-in-out"
         href="#">Кино</a>
-    <a class="px-5 py-2 text-lg bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-full font-semibold transition duration-150 ease-in-out"
+    <a class="text-xl leading-tight border-b-4 border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 font-semibold transition duration-150 ease-in-out"
         href="#">Театр</a>
-    <a class="px-5 py-2 text-lg bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-full font-semibold transition duration-150 ease-in-out"
+    <a class="text-xl leading-tight border-b-4 border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 font-semibold transition duration-150 ease-in-out"
         href="#">Развлечения</a>
-    <a class="px-5 py-2 text-lg bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-full font-semibold transition duration-150 ease-in-out"
+    <a class="text-xl leading-tight border-b-4 border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 font-semibold transition duration-150 ease-in-out"
         href="#">Акции</a>
 </div>
-<div class="container mx-auto mb-32 px-4 grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+<div class="container mx-auto mb-32 px-2 sm:px-6 lg:px-8 grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
     <div class="mb-3">
         <a href="#" class="group">
             <img class="w-full h-auto object-cover group-hover:opacity-90 group-hover:shadow-xl transition duration-150 ease-in-out"
@@ -44,7 +44,7 @@
                 class="mt-3 text-gray-700 group-hover:text-gray-900 text-xl font-bold dark:text-gray-400 dark:group-hover:text-gray-200 transition duration-150 ease-in-out">
                 Конь Юлий и большие скачки</div>
             <div
-                class="text-base font-medium  text-gray-500 group-hover:text-gray-700 dark:text-gray-500 dark:group-hover:text-gray-400 transition duration-150 ease-in-out">
+                class="text-base font-medium text-gray-500 group-hover:text-gray-700 dark:text-gray-500 dark:group-hover:text-gray-400 transition duration-150 ease-in-out">
                 Мультфильм</div>
         </a>
     </div>
